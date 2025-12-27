47034e342a96c1e96f2c9de8347155ce430df3f2
Arman-2107046 <armanr.rafi@gmail.com>
2025-12-27 12:55:22 +0600

    Gumbazar Version 01

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,19 +2,27 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<User>
  */
 class UserFactory extends Factory
 {
     /**
+     * The model that the factory corresponds to.
+     *
+     * REQUIRED for Laravel 8 to properly initialize Faker
+     */
+    protected $model = User::class;
+
+    /**
      * The current password being used by the factory.
      */
-    protected static ?string $password;
+    protected static ?string $password = null;
 
     /**
      * Define the model's default state.
@@ -24,8 +32,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
